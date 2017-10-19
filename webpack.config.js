6d642f40d9925e3c8771d91311a488562d702146
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2017-10-19 23:51:06 +0300

    Travis fix

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = [
     plugins: [
       new ExtractTextPlugin('[name].css'),
     ],
-    watch: true,
+    watch: false,
   },
   {
     entry: {
@@ -65,6 +65,6 @@ module.exports = [
       path: path.resolve(__dirname, 'source/views'),
       libraryTarget: 'umd',
     },
-    watch: true,
+    watch: false,
   },
 ];
