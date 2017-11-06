8f8ecca2411ec5ba28d2ded7f4659a6a6791958d
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2017-11-06 13:01:19 +0300

    More logs

--- source/auth.js
+++ source/auth.js
@@ -3,8 +3,10 @@ const passport = require('koa-passport');
 const looger = require('../libs/logger.js');
 const UsersModel = require('./models/users.js');
 
+const log = looger.child({ component: 'auth' });
+
 const Users = new UsersModel({
-  logger: looger.child({ component: 'auth' }),
+  logger: log,
 });
 
 passport.serializeUser((user, done) => done(null, user.id));
@@ -33,20 +35,32 @@ passport.use(
       callbackURL: process.env.YANDEX_CALLBACK_URL || '/auth/yandex/callback',
     },
     (async (token, tokenSecret, profile, done) => {
+      log.debug('Yandex OAuth success');
+      log.trace(profile);
+
       try {
         const existingUser = await Users.findOne({ yandex_id: profile.id });
 
         if (existingUser) {
+          log.debug(`Found an existing user. ID: ${existingUser.id}`);
           return done(null, existingUser);
         }
 
-        const newUser = await Users.create({
+        log.debug('User not found. Let\'s create...');
+
+        const newUserData = {
           login: profile.username,
           // eslint-disable-next-line
           name: profile._json.real_name || profile.displayName,
           email: profile.emails[0].value,
           yandex_id: profile.id,
-        });
+        };
+
+        log.trace(newUserData);
+
+        const newUser = await Users.create(newUserData);
+
+        log.debug(`User created. ID: ${newUser.id}`);
 
         return done(null, newUser);
       } catch (err) {
@@ -66,12 +80,18 @@ passport.use(
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     (async (token, tokenSecret, profile, done) => {
+      log.debug('Google OAuth success');
+      log.trace(profile);
+
       try {
         const existingUser = await Users.findOne({ google_id: profile.id });
 
         if (existingUser) {
+          log.debug(`Found an existing user. ID: ${existingUser.id}`);
           return done(null, existingUser);
         }
+
+        log.debug('User not found. Let\'s create...');
 
         let { emails } = profile;
 
@@ -79,12 +99,16 @@ passport.use(
           emails = emails.filter((v) => (v.type === 'account'));
         }
 
-        const newUser = await Users.create({
+        const newUserData = {
           login: emails[0].value,
           name: profile.displayName,
           email: emails[0].value,
           google_id: profile.id,
-        });
+        };
+
+        log.trace(newUserData);
+
+        const newUser = await Users.create(newUserData);
 
         return done(null, newUser);
       } catch (err) {
--- source/controllers/logout.js
+++ source/controllers/logout.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   handler: async (ctx) => {
+    ctx.log.debug(`User ID: ${ctx.state.user.id} logout... `);
     ctx.logout();
     ctx.redirect('/');
   },
